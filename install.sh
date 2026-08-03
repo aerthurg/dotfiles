725b4f8400009dc3eca84edfc68aff7be32c307e
Arthur Gonsales <aerthurg@posteo.com>
2026-08-03 01:04:20 -0300

    added dev env install

--- install.sh
+++ install.sh
@@ -16,5 +16,17 @@ chmod +x "$DOTFILES_DIR"/scripts/*.sh
 "$DOTFILES_DIR/scripts/02-system-setup.sh"
 "$DOTFILES_DIR/scripts/03-stow.sh"
 
+log_info "Finishing development environment installation..."
+
+echo 'eval "$(starship init bash)"'
+echo 'eval "$(mise activate bash)"'
+mise install
+
+log_success "Development environment successfully installed!"
+
+log_info "Setting dark theme preference..."
+
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
 log_success "Post-installation finished successfully!"
 log_warning "Reboot your system!"
