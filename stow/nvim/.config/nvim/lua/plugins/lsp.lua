000e724c5b9b21030e88546d9b47c7deb4b8644f
Arthur Gonsales <aerthurg@posteo.com>
2026-09-24 15:00:12 -0300

    removed ruby

---
+++ stow/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,16 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      tailwindcss = {
+        init_options = {
+          userLanguages = {
+            elixir = "html-eex",
+            eelixir = "html-eex",
+            heex = "html-eex",
+          },
+        },
+      },
+    },
+  },
+}
