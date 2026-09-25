367d9916ff47d05eb8e8ad3f53a5e94894d865af
Arthur Gonsales <aerthurg@posteo.com>
2026-09-25 20:15:58 -0300

    updated avante

--- stow/nvim/.config/nvim/lua/plugins/avante.lua
+++ stow/nvim/.config/nvim/lua/plugins/avante.lua
@@ -6,9 +6,10 @@ return {
       provider = "deepseek",
       providers = {
         deepseek = {
+          __inherited_from = "openai",
           api_key_name = "DEEPSEEK_API_KEY",
           endpoint = "https://api.deepseek.com",
-          model = "deepseek-coder",
+          model = "deepseek-chat",
         },
       },
       behaviour = {
